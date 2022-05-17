28f3cbd5bf08fbaf1d09e8bc7f8f805967fc3393
raoul <raoul.rogojinaru@outlook.com>
2022-05-17 18:56:57 +0300

    PCMS-1 Create veterinarian.

--- src/main/java/com/sda/raoul/petclinic/Main.java
+++ src/main/java/com/sda/raoul/petclinic/Main.java
@@ -1,13 +1,46 @@
 package com.sda.raoul.petclinic;
 
 import com.sda.raoul.petclinic.controller.VeterinarianController;
+import com.sda.raoul.petclinic.option.UserOption;
 import com.sda.raoul.petclinic.utils.SessionManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         SessionManager.getSessionFactory();
         VeterinarianController veterinarianController = new VeterinarianController();
-        veterinarianController.create();
+        Scanner scanner = new Scanner(System.in);
+
+        UserOption option = UserOption.UNKNOWN;
+        do {
+            UserOption.printAllOptions();
+            System.out.println("Choose an option.");
+            String inputOption = scanner.nextLine();
+            try {
+                int userOption = Integer.parseInt(inputOption);
+                option = UserOption.findBy(userOption).orElse(UserOption.UNKNOWN);
+            } catch (NumberFormatException e) {
+                option = UserOption.UNKNOWN;
+            }
+            System.out.println(option.getPrettyName());
+
+            switch (option) {
+                case ADD_NEW_VET:
+                    veterinarianController.create();
+                    break;
+                case UNKNOWN:
+                    break;
+                case EXIT:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("This option is not implemented.");
+                    break;
+            }
+        } while (!option.equals(UserOption.EXIT));
+
+        SessionManager.shutDown();
 
     }
 }
---
+++ src/main/java/com/sda/raoul/petclinic/option/UserOption.java
@@ -0,0 +1,39 @@
+package com.sda.raoul.petclinic.option;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum UserOption {
+    ADD_NEW_VET("Add new Veterinarian", 1),
+    EXIT("Exit", 999),
+    UNKNOWN("Unknown option, try again", 1000);
+
+    public final String prettyName;
+    private final int optionNumber;
+
+    UserOption(String prettyName, int optionNumber) {
+        this.prettyName = prettyName;
+        this.optionNumber = optionNumber;
+    }
+
+    public String getPrettyName() {
+        return prettyName;
+    }
+
+    public int getOptionNumber() {
+        return optionNumber;
+    }
+
+    public static void printAllOptions() {
+        System.out.println("----------------------------------");
+        Arrays.stream(UserOption.values())
+                .filter(userOption -> !userOption.equals(UserOption.UNKNOWN))
+                .forEach(option -> System.out.println(option.prettyName + " -> " + option.getOptionNumber()));
+    }
+
+    public static Optional<UserOption> findBy(int optionNumber) {
+        return Arrays.stream(UserOption.values())
+                .filter(userOption -> userOption.getOptionNumber() == optionNumber)
+                .findAny();
+    }
+}
